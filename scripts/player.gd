d4612405ce5e2c4440fbf1b0aa4859df074a9320
jwt2706 <jwt2home@gmail.com>
2025-09-09 09:08:09 -0400

    swap layers

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+@export var red_layer: TileMapLayer
+@export var blue_layer: TileMapLayer
+@export var yellow_layer: TileMapLayer
+
 # Movement settings
 @export var max_speed: float = 400.0
 @export var acceleration: float = 1000.0
@@ -12,14 +16,16 @@ extends CharacterBody2D
 # Power mode system
 var current_mode: String = "red"
 
+func _ready() -> void:
+	set_mode("red")
+
 func _physics_process(delta: float) -> void:
-	# --- Mode switching ---
 	if Input.is_action_just_pressed("blue"):
-		current_mode = "blue"
+		set_mode("blue")
 	if Input.is_action_just_pressed("red"):
-		current_mode = "red"
+		set_mode("red")
 	if Input.is_action_just_pressed("yellow"):
-		current_mode = "yellow"
+		set_mode("yellow")
 
 	velocity.y += gravity * delta
 	var direction := Input.get_axis("left", "right")
@@ -40,3 +46,19 @@ func _physics_process(delta: float) -> void:
 				pass
 
 	move_and_slide()
+	
+func set_mode(color) -> void:
+	current_mode = color
+	match color:
+		"red":
+			red_layer.visible = true
+			blue_layer.visible = false
+			yellow_layer.visible = false
+		"blue":
+			red_layer.visible = false
+			blue_layer.visible = true
+			yellow_layer.visible = false
+		"yellow":
+			red_layer.visible = false
+			blue_layer.visible = false
+			yellow_layer.visible = true
