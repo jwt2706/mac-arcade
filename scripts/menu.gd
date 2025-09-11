2674f9d161ccccd502e51cd14873f2073fb9a7e7
jwt2706 <jwt2home@gmail.com>
2025-09-11 10:15:46 -0400

    ads

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -5,3 +5,9 @@ func _on_start_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("start"):
+		_on_start_button_pressed()
+	if event.is_action_pressed("select"):
+		_on_quit_button_pressed()
