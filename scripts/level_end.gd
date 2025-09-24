21f712ffe5e02cff8ca916b249a39bc97023975a
jwt2706 <jwt2home@gmail.com>
2025-09-24 13:59:49 -0400

    asd

--- scripts/level_end.gd
+++ scripts/level_end.gd
@@ -1,10 +1,16 @@
 extends Control
 
+func _ready() -> void:
+	var highest = Globals.current_level + 1
+	if (highest > Globals.highest_level):
+		Globals.highest_level = highest
+	
 func _on_next_button_pressed() -> void:
-	pass # Replace with function body.
+	Globals.current_level += 1
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_replay_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
