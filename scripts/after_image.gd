4a66ddb61033b8c645980c1f7a267d1449736ef7
jwt2706 <jwt2home@gmail.com>
2025-10-01 14:36:12 -0400

    asd

--- scripts/after_image.gd
+++ scripts/after_image.gd
@@ -1,6 +1,11 @@
 extends StaticBody2D
 
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	animated_sprite.play("idle")
+	
+	#collision_shape.disabled = true
+	#await get_tree().create_timer(1.0).timeout
+	#collision_shape.disabled = false
